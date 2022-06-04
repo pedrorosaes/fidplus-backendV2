7ab0a59a913518f9df4946601cb364b23ad97523
Pedro H. Rosaes <pedro_rosaes@hotmail.com>
2022-06-04 13:50:55 -0300

    add : cors

--- src/server.js
+++ src/server.js
@@ -4,14 +4,22 @@ const db = require("./database");
 const handleError = require("./middlewares/handleError");
 const swaggerUi = require("swagger-ui-express");
 const swagerDocs = require("../swagger.json");
+var cors = require("cors");
 
 const port = process.env.PORT || 3000;
+
+const allowedOrigins = "*";
+
+const options = {
+  origin: allowedOrigins,
+};
 
 const app = express();
 
 db.hasConection();
 
 app.use(express.json());
+app.use(cors(options));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagerDocs));
 app.use(routes);
